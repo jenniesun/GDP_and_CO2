320340127b9b20c8a0108cb1e2042fae0efb9188
Jennie Sun <jennieboyasun@gmail.com>
2020-09-30 15:43:34 -0400

    changed plot theme

--- data.py
+++ data.py
@@ -4,4 +4,4 @@ wdi.head()
 wdi2=wdi[['Mortality rate, infant (per 1,000 live births)', 'GDP per capita (constant 2010 US$)', "Country Name"]]
 wdi2
 from plotnine import *
-(ggplot(wdi2,aes(x='GDP per capita (constant 2010 US$)', y='Mortality rate, infant (per 1,000 live births)')) + geom_point())
+(ggplot(wdi2,aes(x='GDP per capita (constant 2010 US$)', y='Mortality rate, infant (per 1,000 live births)')) + geom_point()) + theme_classic()
